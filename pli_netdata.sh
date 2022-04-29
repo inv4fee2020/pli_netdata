99dddc7c6f22f79ef1fd4c9743aa489d54da8ba6
inv4fee2020 <inv4fee2020@gmail.com>
2022-04-29 19:22:37 +0100

    initial

--- pli_netdata.sh
+++ pli_netdata.sh
@@ -12,7 +12,7 @@ NC='\033[0m' # No Color
 # Authenticate sudo perms before script execution to avoid timeouts or errors
 sudo -l > /dev/null 2>&1
 
-
+# space seperated list
 REQ_PACKAGES=(git curl nano)
 
 
@@ -28,7 +28,7 @@ FUNC_PKG_CHECK(){
            echo >&2 "package "$i" not found. installing...."
            sudo apt install -y "$i"
         fi
-        echo "packages "$i" exist. proceeding...."
+        echo "package "$i" exists. proceeding...."
     done
 
 }
@@ -37,6 +37,8 @@ FUNC_PKG_CHECK(){
 
 
 FUNC_GET_CLAIMTOKEN(){
+
+    FUNC_PKG_CHECK;
 
     echo -e "${GREEN}#########################################################################"
     echo -e "${GREEN}## PROMPT FOR NETDATA SPACE CLAIM-TOKEN...${NC}"
@@ -63,6 +65,22 @@ FUNC_SETUP_NETDATA(){
 
 
 
+FUNC_RECLAIM_TOKEN(){
+    
+    echo "performing node reclaim process"
+    FUNC_EXIT;
+}
+
+
+
+FUNC_RESET_NETDATA(){
+    
+    echo "performing netdata reset process"
+    FUNC_EXIT;
+}
+
+
+
 
 FUNC_EXIT(){
 	exit 0
@@ -81,5 +99,37 @@ FUNC_ERR_EXIT(){
 }
 
 
-FUNC_PKG_CHECK
-FUNC_GET_CLAIMTOKEN
+#FUNC_PKG_CHECK
+#FUNC_GET_CLAIMTOKEN
+
+
+case "$1" in
+        -setup)
+                #_OPTION="-setup"
+                FUNC_GET_CLAIMTOKEN
+                ;;
+        -reclaim)
+                #_OPTION="-conf"
+                FUNC_RECLAIM_TOKEN
+                ;;
+        -reset)
+                #_OPTION="-conf"
+                FUNC_RESET_NETDATA
+                ;;
+        *)
+                #clear
+                echo 
+                echo 
+                echo -e "${GREEN}Usage: $0 {function}${NC}"
+                echo 
+                echo -e "${GREEN}where {function} is one of the following;${NC}"
+                echo 
+                echo -e "${GREEN}      -setup      ==  prompts for claim token id & installs netdata${NC}"
+                echo -e "${GREEN}      -reclaim    ==  removes the unique id to allow the node to be claimed again${NC}"
+                echo 
+                echo -e "${GREEN}      -reset      ==  **CAUTION** performs a hard reset of the netdata install removing all files${NC}"
+                #echo -e "${GREEN}      -scp       ==  displays the secure copy (scp) cmds to download backup files${NC}"
+                #echo
+                echo 
+                echo 
+esac
